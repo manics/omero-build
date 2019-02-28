c04142efc493efd63868abedcdcb5c8259213e76
Simon Li <spli@dundee.ac.uk>
2019-02-28 16:38:05 +0000

    Forcibly disable javadoc
    Top-level gradle publishToMavenLocal -x test -x javadoc doesn't disable javadoc in subprojects

--- gitlab/patch-repositories.sh
+++ gitlab/patch-repositories.sh
@@ -2,7 +2,11 @@
 
 set -eux
 
-#for f in ../omero-*/build.gradle; do patch $f build-gradle.patch; done
-for f in ../omero-*/build.gradle; do (cd $(dirname $f) && git checkout .); done
-#for f in ../omero-*/build.gradle; do cat publishing-gradle.txt >> $f; done
-for f in ../omero-*/build.gradle; do echo '\n\napply from: "../gitlab/publish.gradle"' >> $f; done
+# Reset checkout
+for f in omero-*/build.gradle; do (cd $(dirname $f) && git checkout .); done
+
+# Disable javadoc
+for f in omero-*/build.gradle; do printf '\ntasks.withType(Javadoc).all { enabled = false }\n' >> $f; done
+
+# Include gitlab publish config
+for f in omero-*/build.gradle; do printf '\n\napply from: "../gitlab/publish.gradle"\n' >> $f; done
