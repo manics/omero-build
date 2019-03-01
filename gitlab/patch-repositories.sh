1bac2699129289dad67f7831ea1cde94cc34e3ca
Simon Li <spli@dundee.ac.uk>
2019-03-01 15:59:52 +0000

    Use gradle publish

--- gitlab/patch-repositories.sh
+++ gitlab/patch-repositories.sh
@@ -11,5 +11,8 @@ for f in omero-*/build.gradle; do printf '\ntasks.withType(Javadoc).all { enable
 # Include gitlab publish config
 for f in omero-*/build.gradle; do printf '\n\napply from: "../gitlab/publish.gradle"\n' >> $f; done
 
+# OS X: Use gsed
+SED=gsed
+gsed --version || SED=sed
 # Add https://gitlab.com/manics/omero-gradle-plugins/-/packages
-for f in omero-*/settings.gradle; do sed -i '/gradlePluginPortal()/ a maven {\n url "https://gitlab.com/api/v4/projects/11095150/packages/maven"\n credentials(HttpHeaderCredentials) {\n name = "Job-Token"\n value = System.getenv("CI_JOB_TOKEN") }\n authentication { header(HttpHeaderAuthentication) }\n }' $f; done
+for f in omero-*/settings.gradle; do $SED -i '/gradlePluginPortal()/ a maven {\n url "https://gitlab.com/api/v4/projects/manics%2Fomero-gradle-plugins/packages/maven"\n credentials(HttpHeaderCredentials) {\n name = "Job-Token"\n value = System.getenv("CI_JOB_TOKEN") }\n authentication { header(HttpHeaderAuthentication) }\n }' $f; done
