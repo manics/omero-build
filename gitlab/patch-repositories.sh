5ef54c59768856d18169590907e4925d483c9082
Simon Li <spli@dundee.ac.uk>
2019-03-01 15:35:38 +0000

    Attempt to build using manics/omero-gradle-plugins artifacts

--- gitlab/patch-repositories.sh
+++ gitlab/patch-repositories.sh
@@ -10,3 +10,6 @@ for f in omero-*/build.gradle; do printf '\ntasks.withType(Javadoc).all { enable
 
 # Include gitlab publish config
 for f in omero-*/build.gradle; do printf '\n\napply from: "../gitlab/publish.gradle"\n' >> $f; done
+
+# Add https://gitlab.com/manics/omero-gradle-plugins/-/packages
+for f in omero-*/settings.gradle; do sed -i '/gradlePluginPortal()/ a maven {\n url "https://gitlab.com/api/v4/projects/11095150/packages/maven"\n credentials(HttpHeaderCredentials) {\n name = "Job-Token"\n value = System.getenv("CI_JOB_TOKEN") }\n authentication { header(HttpHeaderAuthentication) }\n }' $f; done
